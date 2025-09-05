15bb1e52a9aec72bf3f9e6f8abce769c69c79c5f
Kruthii-N <kruthiijin@gmail.com>
2025-09-05 16:02:56 +0530

    added inheritance feature

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,7 +1,13 @@
 package book;
 
 public class Book {
-	String book;
+	String title;
 	String author;
 	String isbn;
+	
+	Book(String title, String author, String isbn) {
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+	}
 }
---
+++ src/book/Library.java
@@ -0,0 +1,15 @@
+package book;
+
+public class Library extends Book {
+	Library(String title, String author, String isbn) {
+		super(title, author, isbn);
+	}
+	
+	void issueBook() {
+		// write later
+	}
+	
+	void returnBook() {
+		// write later
+	}
+}
